529fce74b957df65ae69f0d0af2cfd88404e5436
Akansha2004pandey <124514156+Akansha2004pandey@users.noreply.github.com>
2023-10-08 13:28:43 +0530

    Update script.js
    update

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-
 function calculateChange(event) {
     event.preventDefault();
-    const change1= document.getElementById('change');
-    const change2=change1.value;
-    console.log(change2);
-    const routedistance1=document.getElementById('routeDistance');
+    
+    
+         const routedistance1=document.getElementById('routeDistance');
          const routeDistance2=routedistance1.value;
          const routetimes1=document.getElementById('routetimes');
          const routetimes2=routetimes1.value;
@@ -13,24 +11,32 @@ function calculateChange(event) {
          if(years2>5){
             document.querySelector('.show1').innerHTML="Traction system repair required ! Warning";
          }else{
-            document.querySelector('.show1').innerHTML="Safe!";
+            document.querySelector('.show1').innerHTML="Repair is not required ";
          }
          const dis=routeDistance2*routetimes2;
          if(dis>500000){
             document.querySelector('.show2').innerHTML="Get the Gearbox checked ! Warning";
          }else{
-            document.querySelector('.show2').innerHTML="Safe!";
+            document.querySelector('.show2').innerHTML="There are many more miles you can safely drive.";
          }
          if(dis>35000){
             document.querySelector('.show3').innerHTML="Get the brakes checked ! Warning";
          }else{
-            document.querySelector('.show3').innerHTML="Safe!";
+            document.querySelector('.show3').innerHTML="No need to change the brakes";
          }
          
-
-    if (change2 ==="no") {
-        const tyreChange1 = document.getElementById('tyrechange');
-        const tyreChange2=tyreChange1.value;
+     const changed1= document.getElementsByName('change');
+     var done;
+    for (var i = 0; i < changed1.length; i++) {
+                if (changed1[i].checked){
+                    done=changed1[i].value;
+                    break;
+                }
+                        
+            
+        }
+    if (done ==="no") {
+        
         const routedistance1=document.getElementById('routeDistance');
          const routeDistance2=routedistance1.value;
          const routetimes1=document.getElementById('routetimes');
@@ -39,19 +45,19 @@ function calculateChange(event) {
          const rear_distance_miles=15000;
 
          const travelled=routeDistance2*routetimes2;
+
          if(travelled>=front_distance_miles){
-                document.querySelector('.show1').innerHTML="Get the front tyres repaired!";
+                document.querySelector('.show4').innerHTML="Get the front tyres and rear tyres repaired";
          }
          if(travelled<front_distance_miles && travelled>=rear_distance_miles){
-            document.querySelector('.show1').innerHTML="Get the rear tyres repaired!";
+            document.querySelector('.show4').innerHTML="Get the rear tyres repaired but no need to change the front tyres.";
          }
          if(travelled<rear_distance_miles){
-            document.querySelector('.show1').innerHTML="No need to worry"
+            document.querySelector('.show4').innerHTML="It is safe to use the vehicle . No need to change tyres."
          }
-    
-
-}
-else{
+        
+    }
+    else{
             const inputDate = new Date(document.getElementById("tyrechange").value);
 
             // Get the current date
@@ -64,8 +70,11 @@ else{
             const daysPassed = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
 
             // Display the result
-            //document.getElementById("result").innerHTML = `Days passed: ${daysPassed}`;  ignore
+        
         }
-}
-const changeForm = document.getElementById('ChangeForm');
-changeForm.addEventListener('submit', calculateChange);
+
+
+    }
+    const changeForm = document.getElementById('ChangeForm');
+    changeForm.addEventListener('submit', calculateChange);
+    
