4c8d1237a7aaabb36aeabddbf98d52d4cf866b2b
Akansha2004pandey <124514156+Akansha2004pandey@users.noreply.github.com>
2023-10-08 11:37:45 +0530

    Update script.js
    added functionality for brakes , gearbox etc

--- script.js
+++ script.js
@@ -4,7 +4,29 @@ function calculateChange(event) {
     const change1= document.getElementById('change');
     const change2=change1.value;
     console.log(change2);
-    
+    const routedistance1=document.getElementById('routeDistance');
+         const routeDistance2=routedistance1.value;
+         const routetimes1=document.getElementById('routetimes');
+         const routetimes2=routetimes1.value;
+         const years_passed=document.getElementById('years');
+         const years2=years_passed.value;
+         if(years2>5){
+            document.querySelector('.show1').innerHTML="Traction system repair required ! Warning";
+         }else{
+            document.querySelector('.show1').innerHTML="Safe!";
+         }
+         const dis=routeDistance2*routetimes2;
+         if(dis>500000){
+            document.querySelector('.show2').innerHTML="Get the Gearbox checked ! Warning";
+         }else{
+            document.querySelector('.show2').innerHTML="Safe!";
+         }
+         if(dis>35000){
+            document.querySelector('.show3').innerHTML="Get the brakes checked ! Warning";
+         }else{
+            document.querySelector('.show3').innerHTML="Safe!";
+         }
+         
 
     if (change2 ==="no") {
         const tyreChange1 = document.getElementById('tyrechange');
@@ -18,13 +40,13 @@ function calculateChange(event) {
 
          const travelled=routeDistance2*routetimes2;
          if(travelled>=front_distance_miles){
-                document.querySelector('.show').innerHTML="Get the front tyres repaired!";
+                document.querySelector('.show1').innerHTML="Get the front tyres repaired!";
          }
          if(travelled<front_distance_miles && travelled>=rear_distance_miles){
-            document.querySelector('.show').innerHTML="Get the rear tyres repaired!";
+            document.querySelector('.show1').innerHTML="Get the rear tyres repaired!";
          }
          if(travelled<rear_distance_miles){
-            document.querySelector('.show').innerHTML="No need to worry"
+            document.querySelector('.show1').innerHTML="No need to worry"
          }
     
 
